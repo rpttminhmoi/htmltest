d2c807a517633d7d76cc6c7fca6dc9ab2ea03a0a
rpttminhmoi <kudokaito12345@gmail.com>
2024-08-08 14:02:35 +0700

    kocogi

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-function sayHello() {
-    alert("Hello, World!");
+// script.js
+setInterval(setClock, 1000);
+
+const hourHand = document.getElementById('hour');
+const minuteHand = document.getElementById('minute');
+const secondHand = document.getElementById('second');
+
+function setClock() {
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio);
 }
+
+function setRotation(element, rotationRatio) {
+    element.style.transform = `rotate(${rotationRatio * 360}deg)`;
+}
+
+setClock();
